f5e667a1b238ad8a059e52ed517a1d804069a839
Spiller <spillerrec@hotmail.com>
2024-04-29 23:43:02 +0200

    Documentation for AComparator

--- src/comparators/AComparator.hpp
+++ src/comparators/AComparator.hpp
@@ -43,16 +43,51 @@ struct ImageOffset{
 	static double calculate_overlap( Point<> offset, const Plane& img1, const Plane& img2 );
 };
 
+/// Interface for comparing two images and finding alignment between them.
+/// AAligner will use the AComparator stored in AContainer to align more than two images.
+/// To support comparing in different domains (such as doing edge detection first), process/processAlpha is used before the
+/// result is passed to findOffset.
+/// This might need some more consideration on how it should work
 class AComparator{
 	public:
+		/// @brief Scaling for sub-pixel alignment. `process()` will return an image scaled by this amount
+		/// @return The scaling factors for sub-pixel alignment
 		virtual Size<double> scale() const { return { 1.0, 1.0 }; }
+
+		/// @brief Optionally pre-processes the input plane before aligning.
+		/// @param plane The input plane. Must stay alive as long as you hold a reference to the returned ModifiedPlane
+		/// @return The input plane with any pre-processing if needed.
 		virtual ModifiedPlane process( const Plane& plane ) const { return { plane }; }
+
+		/// @brief Corresponding `process()` for the alpha channel. This should be overloaded if you transform the coordinate space,
+		/// for example by scaling the images. 
+		/// @param plane The input alpha plane. Must stay alive as long as you hold a reference to the returned ModifiedPlane
+		/// @return An alpha plane compatible with the result from `process()`
 		virtual ModifiedPlane processAlpha( const Plane& plane ) const { return { plane }; }
+
+		/// @brief Finds the alignment between two images
+		/// @param img1 Image which will be the reference for the alignment
+		/// @param img2 The image which we want to find how much has been transformed
+		/// @param a1 Optional cooresponding alpha plane to `img1`
+		/// @param a2 Optional cooresponding alpha plane to `img2`
+		/// @param hint Initial guess of how the images are aligned, which can speed up some algorithms
+		/// @return An `ImageOffset` containing the result of the alignment detection
 		virtual ImageOffset findOffset( const Plane& img1, const Plane& img2, const Plane& a1, const Plane& a2, Point<double> hint = {0.0,0.0} ) const = 0;
+
 		virtual ~AComparator() = default;
 		
+		/// @brief Gives an error value for two images at a specific translation in an unspecifed metric specific to this Comparator.
+		/// @todo Suppport rotation/scaling
+		/// @param img1 Base image (offset 0,0)
+		/// @param img2 Compare image (offset `x`,`y`)
+		/// @param a1 Optional alpha for `img1`
+		/// @param a2 Optional alpha for `img2`
+		/// @param x The amount of translation in the horizontal direction
+		/// @param y The amount of translation in the vertical direction
+		/// @return The error for this alignment. Larger values mean worse alignment, but the scale is unspecified
 		virtual double findError( const Plane& img1, const Plane& img2, const Plane& a1, const Plane& a2, double x, double y ) const = 0;
 
+		/// @return True if `findOffset` does allow rotation and/or scaling to be detected, or false if only translation is considered
 		virtual bool includesRotationOrScale() const { return false; }
 };
 
