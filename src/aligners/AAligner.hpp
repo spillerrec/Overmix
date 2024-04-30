b1ceebf8268c481a1e1ecf9f69046f10a2b82021
Spiller <spillerrec@hotmail.com>
2024-04-30 23:39:57 +0200

    Documentation of AAligner

--- src/aligners/AAligner.hpp
+++ src/aligners/AAligner.hpp
@@ -27,10 +27,20 @@ enum class AlignMethod{ //TODO: We have this twice
 };
 
 
+/// @brief Base class for all higher-level alignment methods which aligns several images into a single panorama image.
+/// The lower level alignment between two images are handled by AComparator, which AAligner sub-classes should use (if possible)
+/// to align an arbitary amount of images together.
 class AAligner{
 	public:
 		virtual ~AAligner() { }
 		
+		/// @brief Aligns all the images in the specified AContainer. The container must return a valid AComparator
+		/// and some implementations might have further requirements. The initial frame offsets might be preserved,
+		/// but usually they are ignored and completely replaced with the results.
+		/// In order to only align a sub-set of images, wrap the container using a meta AContainer such as FrameContainer
+		/// On failure an exception will be thrown.
+		/// @param container The container with the images to align. Must have a AComparator set
+		/// @param watcher If progress monitoring is wanted, set this to non-null to receive progress updates during this call
 		virtual void align( class AContainer& container, class AProcessWatcher* watcher=nullptr ) const = 0;
 };
 
